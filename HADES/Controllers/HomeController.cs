87ba3d746014718139c4a74eeaded4f0efb813d0
JeanSamGirard <jeansam@live.ca>
2021-04-13 14:07:13 -0400

    Css + Git LFS Default background login

--- HADES/Controllers/HomeController.cs
+++ HADES/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using HADES.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -9,15 +10,14 @@ using System.Threading.Tasks;
 
 namespace HADES.Controllers
 {
-	public class HomeController : Controller
+	public class HomeController : LocalizedController<HomeController>
 	{
+        public HomeController(IStringLocalizer<HomeController> localizer) : base(localizer)
+        {
 
-		public HomeController()
-		{
-			
-		}
+        }
 
-		public IActionResult Login()
+        public IActionResult Login()
 		{
 			return View();
 		}
