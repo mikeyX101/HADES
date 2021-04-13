288ddcddccf2b124b4f27eb2262d5d0f5b262c0f
Samuel Caron <samuel.caron101@gmail.com>
2021-04-13 12:40:41 -0400

    Localization Setup

---
+++ HADES/Controllers/LocalizedController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HADES.Controllers
+{
+	public class LocalizedController<T> : Controller
+	{
+		private readonly IStringLocalizer<T> _localizer;
+		internal IStringLocalizer Localizer => _localizer;
+
+		public LocalizedController(IStringLocalizer<T> localizer)
+		{
+			_localizer = localizer;
+		}
+	}
+}
--- HADES/Startup.cs
+++ HADES/Startup.cs
@@ -4,12 +4,15 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -35,6 +38,52 @@ namespace HADES
 			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
 				.AddEntityFrameworkStores<ApplicationDbContext>();
 			services.AddControllersWithViews();
+
+			#region Localization Setup
+			// Configure localization
+			services.Configure<RequestLocalizationOptions>(options =>
+			{
+				// Add supported cultures here
+				IList<CultureInfo> supportedCultures = new List<CultureInfo>
+				{
+					new CultureInfo("fr-CA"),
+					new CultureInfo("en-US"),
+					new CultureInfo("es-US"),
+					new CultureInfo("pt-BR")
+				};
+				// Set default culture to fr (ServiceCulture and UICulture)
+				options.DefaultRequestCulture = new RequestCulture("fr-CA", "fr-CA");
+				options.SupportedCultures = supportedCultures;
+				options.SupportedUICultures = supportedCultures;
+				/*
+					Culture Providers are used to know which culture to use.
+					If a provider returns null, the next provider in the list is used until a non-null result is returned.
+
+					Default order:
+						Accepted-Language header provider
+						Query string provider (ex: <url>?l=fr-CA)
+						Cookie provider
+
+					We should clear the default providers and only use our own.
+				*/
+				options.RequestCultureProviders.Clear();
+				options.RequestCultureProviders.Add(new CustomRequestCultureProvider(async context => {
+					// Do DB request, context is HttpContext
+					await Task.Delay(1);
+
+					// TEMP Allow override from query string
+					string locale = context.Request.Query["l"].ToString() ?? "fr-CA";
+					// Return culture from request
+					return new ProviderCultureResult(locale);
+				}));
+			});
+
+			// Set resource path for localizations
+			services.AddLocalization(localOptions => { localOptions.ResourcesPath = "App_LocalResources"; });
+
+			services.AddMvc().AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.SubFolder);
+			services.AddMvc().AddDataAnnotationsLocalization();
+			#endregion
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +107,10 @@ namespace HADES
 
 			app.UseAuthentication();
 			app.UseAuthorization();
+
+			// Apply localization service to app
+			IOptions<RequestLocalizationOptions> options = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+			app.UseRequestLocalization(options.Value);
 
 			app.UseEndpoints(endpoints =>
 			{
