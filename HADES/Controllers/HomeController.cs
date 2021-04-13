8ed9a9561eb98d497198c91b6fc1986a69bcbf23
JeanSamGirard <jeansam@live.ca>
2021-04-13 13:25:16 -0400

    Login View

--- HADES/Controllers/HomeController.cs
+++ HADES/Controllers/HomeController.cs
@@ -11,19 +11,13 @@ namespace HADES.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController()
 		{
-			_logger = logger;
+			
 		}
 
-		public IActionResult Index()
-		{
-			return View();
-		}
-
-		public IActionResult Privacy()
+		public IActionResult Login()
 		{
 			return View();
 		}
