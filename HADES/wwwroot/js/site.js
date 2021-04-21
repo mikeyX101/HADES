429dff75a68a4e0b6bc0e707970ff8d5d34b6ba5
roro277gmail <70496082+roro277@users.noreply.github.com>
2021-04-20 22:06:06 -0400

    Treeview

--- HADES/wwwroot/js/site.js
+++ HADES/wwwroot/js/site.js
@@ -1,4 +1,49 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function () {
 
-// Write your JavaScript code.
+    // Data for treeview
+    var defaultData = [
+        {
+            text: 'Dossier 1',
+            href: '#dossier1',
+            nodes: [
+                {
+                    text: 'Groupe 1',
+                    nodes: [
+                        {
+                            text: 'Membre 1',
+                        },
+                        {
+                            text: 'Membre 2',
+                        }
+                    ]
+                },
+                {
+                    text: 'Groupe 2',
+                }
+            ]
+        },
+        {
+            text: 'Dossier 2',
+        },
+        {
+            text: 'Dossier 3',
+        }
+    ];
+
+    // customization options
+    $('#treeview1').treeview({
+        color: "#428bca",
+        expandIcon: 'fa fa-plus',
+        collapseIcon: 'fa fa-minus',
+        showBorder: false,
+        highlightSelected: true,
+        data: defaultData
+    });
+
+    // Action when node is selected
+    $('#treeview1').on('nodeSelected', function (event, data) {
+        window.alert("You have selected: " + data.text + "\n");
+        console.log(data);
+    });
+
+});
