e30d89a08f697218fefa3314fb1b1f5004787223
roro277gmail <70496082+roro277@users.noreply.github.com>
2021-04-21 10:20:27 -0400

    Clean up

--- HADES/Controllers/HomeController.cs
+++ HADES/Controllers/HomeController.cs
@@ -1,12 +1,8 @@
-﻿using HADES.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
+using HADES.Util;
+using HADES.Models;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HADES.Controllers
 {
@@ -25,6 +21,9 @@ namespace HADES.Controllers
         public IActionResult MainView(/*Folder f*/)
         {
             // Fill ViewBag with Folders and Groups to display as a TreeSet
+            var adManager = new ADManager();
+            var root = adManager.getRoot();
+           
             return View();
         }
 
---
+++ HADES/wwwroot/js/treeview.js
@@ -0,0 +1,52 @@
+﻿$(function () {
+
+    // Data for treeview
+    var defaultData = [
+        {
+            text: 'Dossier 1',
+            nodes: [
+                {
+                    text: 'Groupe 1',
+                    nodes: [
+                        {
+                            text: 'Membre 1',
+                        },
+                        {
+                            text: 'Membre 2',
+                        }
+                    ]
+                },
+                {
+                    text: 'Groupe 2',
+                }
+            ]
+        },
+        {
+            text: 'Dossier 2',
+        },
+        {
+            text: 'Dossier 3',
+        }
+    ];
+
+    // options
+    $('#mytreeview').treeview({
+        color: "#428bca",
+        expandIcon: 'fa fa-plus',
+        collapseIcon: 'fa fa-minus',
+        showBorder: false,
+        highlightSelected: true,
+        data: defaultData
+    });
+
+    // collapses all nodes
+    $('#mytreeview').treeview('collapseAll', { silent: true });
+
+    // Action when node is selected
+    $('#mytreeview').on('nodeSelected', function (event, data) {
+        // TODO: show node content
+        window.alert("You have selected: " + data.text + "\n");
+        console.log(data);
+    });
+
+});
