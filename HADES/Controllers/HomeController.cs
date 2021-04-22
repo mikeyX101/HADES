7ec0b5915cc1225dd3e83698b1cc235afde32b5d
roro277gmail <70496082+roro277@users.noreply.github.com>
2021-04-22 00:25:32 -0400

    + TreeNode class

--- HADES/Controllers/HomeController.cs
+++ HADES/Controllers/HomeController.cs
@@ -8,14 +8,18 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace HADES.Controllers
 {
     public class HomeController : LocalizedController<HomeController>
     {
+        private ADManager ad;
+
         public HomeController(IStringLocalizer<HomeController> localizer) : base(localizer)
         {
-            ADManager ad = new ADManager();
+            ad = new ADManager();
         }
 
         public IActionResult Login()
@@ -27,6 +31,34 @@ namespace HADES.Controllers
         public IActionResult MainView(/*Folder f*/)
         {
             // Fill ViewBag with Folders and Groups to display as a TreeSet
+            var adRoot = ad.getRoot();
+
+            TreeNode<string> root = new TreeNode<string>("root");
+            {
+                TreeNode<string> node0 = root.AddChild("node0");
+                TreeNode<string> node1 = root.AddChild("node1");
+                TreeNode<string> node2 = root.AddChild("node2");
+                {
+                    TreeNode<string> node20 = node2.AddChild("node20");
+                    TreeNode<string> node21 = node2.AddChild("node21");
+                    {
+                        TreeNode<string> node210 = node21.AddChild("node210");
+                        TreeNode<string> node211 = node21.AddChild("node211");
+                    }
+                }
+                TreeNode<string> node3 = root.AddChild("node3");
+                {
+                    TreeNode<string> node30 = node3.AddChild("node30");
+                }
+            }
+            var rootJson = JsonConvert.SerializeObject(root, Formatting.Indented,
+                                            new JsonSerializerSettings
+                                            {
+                                                NullValueHandling = NullValueHandling.Ignore,
+                                                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                                            });
+            rootJson = rootJson.Replace("\"nodes\": []", "");
+            ViewBag.output = rootJson;
             return View();
         }
 
--- HADES/wwwroot/js/treeview.js
+++ HADES/wwwroot/js/treeview.js
@@ -1,52 +1,101 @@
-﻿$(function () {
+﻿function userDataDependent(userObj) {
+    $(function () {
 
-    // Data for treeview
-    var defaultData = [
-        {
-            text: 'Dossier 1',
-            nodes: [
-                {
-                    text: 'Groupe 1',
-                    nodes: [
-                        {
-                            text: 'Membre 1',
-                        },
-                        {
-                            text: 'Membre 2',
-                        }
-                    ]
-                },
-                {
-                    text: 'Groupe 2',
-                }
-            ]
-        },
-        {
-            text: 'Dossier 2',
-        },
-        {
-            text: 'Dossier 3',
-        }
-    ];
+        // Data for treeview
+        //var defaultData1 = [
+        //    {
+        //        text: 'Dossier 1',
+        //        nodes: [
+        //            {
+        //                text: 'Groupe 1',
+        //                nodes: [
+        //                    {
+        //                        text: 'Membre 1',
+        //                    },
+        //                    {
+        //                        text: 'Membre 2',
+        //                    }
+        //                ]
+        //            },
+        //            {
+        //                text: 'Groupe 2',
+        //            }
+        //        ]
+        //    },
+        //    {
+        //        text: 'Dossier 2',
+        //    },
+        //    {
+        //        text: 'Dossier 3',
+        //    }
+        //];
 
-    // options
-    $('#mytreeview').treeview({
-        color: "#428bca",
-        expandIcon: 'fa fa-plus',
-        collapseIcon: 'fa fa-minus',
-        showBorder: false,
-        highlightSelected: true,
-        data: defaultData
+        //var defaultData2 = [
+        //    {
+        //        "text": "root",
+        //        "nodes": [
+        //            {
+        //                "text": "node0",
+        //                "nodes": []
+        //            },
+        //            {
+        //                "text": "node1",
+        //                "nodes": []
+        //            },
+        //            {
+        //                "text": "node2",
+        //                "nodes": [
+        //                    {
+        //                        "nodes": []
+        //                    },
+        //                    {
+        //                        "text": "node21",
+        //                        "nodes": [
+        //                            {
+        //                                "text": "node210",
+        //                                "nodes": []
+        //                            },
+        //                            {
+        //                                "text": "node211",
+        //                                "nodes": []
+        //                            }
+        //                        ]
+        //                    }
+        //                ]
+        //            },
+        //            {
+        //                "text": "node3",
+        //                "nodes": [
+        //                    {
+        //                        "text": "node30",
+        //                        "nodes": []
+        //                    }
+        //                ]
+        //            }
+        //        ]
+        //    }
+        //];
+
+        var defaultData3 = JSON.parse('[' + JSON.stringify(userObj) + ']');
+
+        // options
+        $('#mytreeview').treeview({
+            color: "#428bca",
+            expandIcon: 'fa fa-plus',
+            collapseIcon: 'fa fa-minus',
+            showBorder: false,
+            highlightSelected: true,
+            data: defaultData3
+        });
+
+        // collapses all nodes
+        $('#mytreeview').treeview('collapseAll', { silent: true });
+
+        // Action when node is selected
+        $('#mytreeview').on('nodeSelected', function (event, data) {
+            // TODO: show node content
+            window.alert("You have selected: " + data.text + "\n");
+        });
+
     });
-
-    // collapses all nodes
-    $('#mytreeview').treeview('collapseAll', { silent: true });
-
-    // Action when node is selected
-    $('#mytreeview').on('nodeSelected', function (event, data) {
-        // TODO: show node content
-        window.alert("You have selected: " + data.text + "\n");
-        console.log(data);
-    });
-
-});
+}
