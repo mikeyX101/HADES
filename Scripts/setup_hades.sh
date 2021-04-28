341ea31fc5fa58b91152127598a1d64d370d2b3f
Samuel Caron <samuel.caron101@gmail.com>
2021-04-28 19:48:19 -0400

    HADES Kestrel Server Linux Service

--- Scripts/setup_hades.sh
+++ Scripts/setup_hades.sh
@@ -2,87 +2,122 @@
 # Samuel Caron
 
 # Tested on CentOS 7.9.2009
-# Script to prepare an SSH key to get HADES from Github and install needed dependencies.
+# Script to prepare an SSH key to get HADES from Github, install needed dependencies and setup the Nginx reverse proxy.
+
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+
+# -----------------------------------------------------------------
+#						www-hades User Setup
+# -----------------------------------------------------------------
+
+# Create app user
+if ! id www-hades >/dev/null 2>&1; then
+	useradd --system www-hades 
+fi
+
+# Make site folder
+if ! [ -d /var/www ]; then 
+	mkdir /var/www
+	chown www-hades /var/www
+	chgrp www-hades /var/www
+fi
 
 # Generate rsa4096 key without passcode
 echo "Generating SSH key for the Github repository..."
-ssh-keygen -q -t rsa -b 4096 -N "" -f ~/.ssh/github_hades_rsa <<< n # Answer no if asking to overwrite
+ssh-keygen -q -t rsa -b 4096 -N "" -f /var/www/github_hades_rsa <<< n # Answer no if asking to overwrite
+chmod 640 /var/www/github_hades_rsa
+chmod 644 /var/www/github_hades_rsa.pub
+chown www-hades /var/www/github_hades_*
+chgrp www-hades /var/www/github_hades_*
 
 # Allow git to use key on Github in SSH config if file or entry doesn't exist
-if ! [ -e ~/.ssh/config ] || ! grep -q "IdentityFile ~/.ssh/github_hades_rsa" ~/.ssh/config; then
-	echo "Modifying user SSH config to allow git to use the SSH key to Github.com..." 
+if ! [ -e /etc/ssh/ssh_config ] || ! grep -q "IdentityFile /var/www/github_hades_rsa" /etc/ssh/ssh_config; then
+	echo "Modifying server SSH config to allow git to use the SSH key to Github.com..." 
 	# Formatted config
 	# Host *
 	#	Hostname github.com
 	#	User git
-	#	IdentityFile ~/.ssh/github_hades_rsa
-	echo $'Host *\n\tHostname github.com\n\tUser git\n\tIdentityFile ~/.ssh/github_hades_rsa' >> ~/.ssh/config
+	#	IdentityFile /var/www/github_hades_rsa
+	echo $'Host *\n\tHostname github.com\n\tUser git\n\tIdentityFile /var/www/github_hades_rsa' >> /etc/ssh/ssh_config
 	
 	# Make sure config is readable
-	chmod 644 ~/.ssh/config
+	#chmod 644 ~/.ssh/config
 fi
 
 # Install git (-q)
-echo "Installing git... (Requires root)"
-sudo yum -y install git
+echo "Installing git..."
+yum -y install git
 
 # -----------------------------------------------------------------
 #							Nginx Setup
 # -----------------------------------------------------------------
 # Add Nginx's package repository
-echo "Installing Nginx's package repository... (Requires root)"
-sudo yum -y install epel-release
+echo "Installing Nginx's package repository..."
+yum -y install epel-release
 
 # Install Nginx (-q)
-echo "Installing Nginx... (Requires root)"
-sudo yum -y install nginx
+echo "Installing Nginx..."
+yum -y install nginx
 
-echo "Configuring Nginx... (Requires root)"
+echo "Configuring Nginx..."
 # https://wiki.centos.org/TipsAndTricks/SelinuxBooleans
 # httpd_can_network_connect (HTTPD Service)
 #    Allow HTTPD scripts and modules to connect to the network. 
-sudo setsebool -P httpd_can_network_connect 1
+setsebool -P httpd_can_network_connect 1
 
-sudo firewall-cmd --permanent --zone=public --add-service=http
-sudo firewall-cmd --permanent --zone=public --add-service=https
+firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-service=https
 
 
 # -----------------------------------------------------------------
 #							DotNet Setup
 # -----------------------------------------------------------------
 # Add Microsoft's CentOS 7 package repository (--quiet)
-sudo rpm -Uvh https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm
+rpm -Uvh https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm
 
 # Install dotnet-sdk-5.0 (-q)
 echo "Installing dotnet-sdk-5.0... (Requires root, can take some time)"
-sudo yum -y install dotnet-sdk-5.0
+yum -y install dotnet-sdk-5.0
 
 # Opt-out of DotNet telemetry
-if ! grep -q "export DOTNET_CLI_TELEMETRY_OPTOUT=1" ~/.bashrc; then
-	echo $'export DOTNET_CLI_TELEMETRY_OPTOUT=1\n' >> ~/.bashrc
-	source ~/.bashrc
-fi
+#if ! grep -q "export DOTNET_CLI_TELEMETRY_OPTOUT=1" ~/.bashrc; then
+#	echo $'export DOTNET_CLI_TELEMETRY_OPTOUT=1\n' >> ~/.bashrc
+#	source ~/.bashrc
+#fi
 
 # -----------------------------------------------------------------
 #							HADES Setup
 # -----------------------------------------------------------------
-echo "Public key available at: ~/.ssh/github_hades_rsa.pub"
+echo "Public key available at: /var/www/github_hades_rsa.pub"
 echo "Public key: "
-cat ~/.ssh/github_hades_rsa.pub
+cat /var/www/github_hades_rsa.pub
 
 read -n1 -rsp $'\nPlease wait for the key to be added in the git repository and press any key to continue...\n'
 
-echo "Cloning HADES in ~/hades..."
-if ! [ -d ~/hades ]; then 
-	mkdir ~/hades
+echo "Cloning HADES in /var/www/hades..."
+if ! [ -d /var/www/hades ]; then 
+	mkdir /var/www/hades
+	chown www-hades /var/www/hades
+	chgrp www-hades /var/www/hades
 fi
-git clone git@github.com:ShaiLynx/HADES.git ~/hades
+git clone git@github.com:ShaiLynx/HADES.git /var/www/hades
+chown -R www-hades /var/www/hades/*
+chgrp -R www-hades /var/www/hades/*
+
+# Copy the HADES Kestrel Server service and start it
+cp /var/www/hades/Scripts/configs/systemd/hades-kestrel-server.service /etc/systemd/system/hades-kestrel-server.service
 
 # Copy new Nginx config and start Nginx
-sudo cp ./configs/nginx/nginxHttps.conf /etc/nginx/nginx.conf
+cp /var/www/hades/Scripts/configs/nginx/nginxHttps.conf /etc/nginx/nginx.conf
 
-echo "Starting Nginx... (Requires root)"
-sudo systemctl enable nginx
+echo "Starting HADES Kestrel Server..."
+systemctl enable hades-kestrel-server.service
+
+echo "Starting Nginx..."
+systemctl enable nginx
 
 echo "Please specify your SSL Certificate and your server name in the Nginx config file at: /etc/nginx/nginx.conf"
 echo "and restart Nginx."
