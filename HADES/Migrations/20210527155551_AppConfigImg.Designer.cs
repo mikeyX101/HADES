50fb9ffef9bc3051c5456ca8903f89e9677a4f65
JeanSamGirard <jeansam@live.ca>
2021-05-27 12:06:28 -0400

    Attention le Seed est lourd

--- HADES/Migrations/20210527155551_AppConfigImg.Designer.cs
+++ HADES/Migrations/20210527155551_AppConfigImg.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using HADES.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HADES.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210527155551_AppConfigImg")]
+    partial class AppConfigImg
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
