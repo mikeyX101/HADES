53a99be49b03ceb11aa766e1efa11a9274fc6761
Samuel Caron <samuel.caron101@gmail.com>
2021-06-01 16:11:13 -0400

    More localisation, fixed responsive datatable

--- HADES/Models/Email.cs
+++ HADES/Models/Email.cs
@@ -14,26 +14,32 @@ namespace HADES.Models
         [Required]
         [EmailAddress]
         [Column("EMA_email")]
+        [Display(Name = "Address")]
         public string Address { get; set; }
 
         [Required]
         [Column("EMA_expiration_date")]
+        [Display(Name = "ExpirationDate")]
         public bool ExpirationDate { get; set; }
 
         [Required]
         [Column("EMA_group_create")]
+        [Display(Name = "GroupCreate")]
         public bool GroupCreate { get; set; }
 
         [Required]
         [Column("EMA_group_delete")]
+        [Display(Name = "GroupDelete")]
         public bool GroupDelete { get; set; }
 
         [Required]
         [Column("EMA_member_add")]
+        [Display(Name = "MemberAdd")]
         public bool MemberAdd { get; set; }
 
         [Required]
         [Column("EMA_member_remove")]
+        [Display(Name = "MemberRemoval")]
         public bool MemberRemoval { get; set; }
 
         [ForeignKey("UserConfig")]
