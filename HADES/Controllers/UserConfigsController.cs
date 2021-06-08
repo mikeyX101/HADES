98a3aaf99b13e31c5ee1585bf8617888f5b3af7c
roro277gmail <70496082+roro277@users.noreply.github.com>
2021-06-08 15:16:05 -0400

    UserConfig view Bug fix : exception if no emails

--- HADES/Controllers/UserConfigsController.cs
+++ HADES/Controllers/UserConfigsController.cs
@@ -80,7 +80,8 @@ namespace HADES.Controllers
 
         private bool AreEmailAddressesUnique(UserConfigViewModel viewModel)
         {
-            return viewModel.Emails.Select(item => item.Address).Distinct().Count() == viewModel.Emails.Select(item => item.Address).Count();
+            return viewModel.Emails != null ? 
+                viewModel.Emails.Select(item => item.Address).Distinct().Count() == viewModel.Emails.Select(item => item.Address).Count() : true;
         }
 
         [Authorize]
